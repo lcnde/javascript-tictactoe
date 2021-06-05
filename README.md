7c6f412ea566e10aee7d0af54dbcacb3e507a320
lcnde <176673026+lcnde@users.noreply.github.com>
2021-06-05 18:30:13 +0200

    update

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ Tic tac toe made in javascript to practice DOM manipulation, modules,
 scopes, closures, factories
 
 
+Player 1 will start the first turn, then Player 2 will start the second turn (and so on)
+
+Make a pop up button under the Players that will appear when the turn is finished and one of the 2
+players has won
+
+Make the player 1 or 2 name light up based on who's turn it is
+
+
--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+/* 
+Rule of thumb:
+  If you only need ONE of something (gameBoard, displayController) use a module;
+  If you need multiples of something (players) create them with factories;
+  Have as little global code as possible;
+*/
+const player = (name, score) => {
+
+  const mark = () => {
+    var spot = document.querySelector(".space");
+    spot.addEventListener('click', () => {
+      spot.classList.add("spacex");
+    });
+  };
+
+
+  return { name, score, mark }
+};
+
+var spot = document.querySelectorAll(".space");
+spottable = Array.from(spot);
+console.log(spottable)
+
+spottable.forEach(function(element) {
+  element.addEventListener('click', () => {
+    element.classList.add("spacex");
+    });
+});
+
+/*use a module as a game that will keep track of evereything. when you make moves with your players
+update the game variables, so you know when a player wins or loses, and when to assign points*/
 
